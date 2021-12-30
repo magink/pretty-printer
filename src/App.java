a71c4dcefa1dccc93611bb2473386d3c8cf82780
magink <magink1991@gmail.com>
2021-12-30 12:30:39 +0100

    refactor: longer sentence

--- src/App.java
+++ src/App.java
@@ -6,7 +6,12 @@ import com.github.magink.pretty_printer.PrettyPrinter;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String toTokenize = "I want to parse this string. Are you sure? YES I AM! ";
+        String toTokenize = 
+        "What is a contact microphone?" +  
+        "A contact mic records sounds from inside things. Like this ice." +
+        "Your mangled brain would like you to know that there is a boxer called Contact Mike" +
+        "Yeah? Any news on my wife name? How about my mother?" + 
+        "Nope. You are welcome!";
         TokenReceiver tokenAdapter = new TokenizerAdapter(toTokenize);
         Parser parser = new Parser(tokenAdapter);
         PrettyPrinter printer = new PrettyPrinter(parser);
